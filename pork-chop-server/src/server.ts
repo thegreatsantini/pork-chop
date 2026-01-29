93805e1fbf1fca55e5d2b0f66fc7f09c9bbdeac3
Lucas Santini <lsantini@Lucass-MacBook-Pro.local>
2026-01-29 16:09:01 -0500

    set up live stream

--- pork-chop-server/src/server.ts
+++ pork-chop-server/src/server.ts
@@ -1,8 +1,38 @@
 import express from 'express';
 import { WebSocketServer } from 'ws';
 import PoweredUP from 'node-poweredup';
+import { spawn } from 'child_process'
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = 3001;
+
+app.use('/stream', express.static(path.join(__dirname, 'public/stream')));
+
+const ffmpeg = spawn('ffmpeg', [
+  '-rtsp_transport', 'tcp',
+  '-i', 'rtsp://admin:AslanBear@192.168.68.138:554/h264Preview_01_main',
+  '-c:v', 'copy', // Copy video codec (no re-encoding = faster)
+  '-f', 'hls',
+  '-hls_time', '2',
+  '-hls_list_size', '3',
+  '-hls_flags', 'delete_segments+append_list',
+  '-hls_segment_filename', 'public/stream/segment%03d.ts',
+  'public/stream/stream.m3u8'
+]);
+
+ffmpeg.stderr.on('data', (data: any) => {
+  console.log(`ffmpeg: ${data}`);
+});
+
+ffmpeg.on('close', (code: any) => {
+  console.log(`ffmpeg exited with code ${code}`);
+});
+
 
 const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
